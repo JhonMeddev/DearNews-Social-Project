ad2311321cd6da031336fe758f5ffcf419b12473
Jhon Med <jhonatan1medeiros@gmail.com>
2022-01-20 15:34:19 -0300

    adicionando rotas

--- frontend/dearNews/src/app/app-routing.module.ts
+++ frontend/dearNews/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MenuComponent } from './menu/menu.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
@@ -5,8 +6,9 @@ import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
 
-    {path: '', redirectTo: 'register', pathMatch: 'full'},
-  
+    {path: '', redirectTo: 'menu', pathMatch: 'full'},
+
+    {path:'menu', component: MenuComponent},
     {path:'login', component: LoginComponent},
     {path :'register', component: RegisterComponent}
 

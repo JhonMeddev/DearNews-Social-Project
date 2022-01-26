b3dd2d5cb94090a0dfe2e2dd3d19c657a849b39a
Jhon Med <jhonatan1medeiros@gmail.com>
2022-01-26 15:57:40 -0300

    update

--- frontend/dearNews/src/app/menu/menu.component.ts
+++ frontend/dearNews/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public auth: AuthService,
+  ) { }
 
   ngOnInit(): void {
   }

c6b8bde5c1675b96ad80498356da5de92f8f7cc0
Rubem <rubem.martinsf@gmail.com>
2022-02-01 14:07:08 -0300

    delete-post

--- frontend/dearNews/src/app/delete/delete-post/delete-post.component.ts
+++ frontend/dearNews/src/app/delete/delete-post/delete-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/service/post.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Post } from 'src/app/model/Post';
 
 @Component({
   selector: 'app-delete-post',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeletePostComponent implements OnInit {
 
-  constructor() { }
+  post: Post = new Post
+  idPost: number
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postService: PostService,
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    this.idPost = this.route.snapshot.params['id']
+    this.findByIdPostagem(this.idPost)
+
+  }
+
+  findByIdPostagem(id: number){
+    this.postService.getByIdPosts(id).subscribe((resp: Post)=>{
+      this.post = resp
+    })
+  }
+
+  delete(){
+   this.postService.deletePost(this.idPost).subscribe(()=>{
+     alert('Postagem apagada com sucesso')
+     this.router.navigate(['/inicio'])
+   })
   }
 
 }

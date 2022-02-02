1208b36726537a8ca37f8c7d853ef038f5198416
Geovani Ferreira <gugaheadbanger@gmail.com>
2022-02-02 14:19:37 -0300

    html e ts de edit-user prontos

--- frontend/dearNews/src/app/edit/edit-user/edit-user.component.ts
+++ frontend/dearNews/src/app/edit/edit-user/edit-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserModel } from 'src/app/model/UserModel';
+import { AuthService } from 'src/app/service/auth.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  user: UserModel = new UserModel();
+  idUser: number;
+  confirmarSenha: string;
+  tipoUsuario: string;
+  
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
+
+    if (environment.token == "") {
+      this.router.navigate(["/logar"])
+    }
+    this.idUser = this.route.snapshot.params["id"];
+    this.findByIdUser(this.idUser);
+  }
+
+  confirmaSenha(event: any) {
+    this.confirmarSenha = event.target.value;
+  }
+
+  tipoUser(event: any) {
+    this.tipoUser = event.target.value;
+  }
+
+  atualizar() {
+    this.user.tipo = this.tipoUsuario;
+    console.log(this.user)
+    if(this.user.password != this.confirmarSenha) {
+      alert("Senhas divergentes.");
+    }else {
+      this.authService.cadastrar(this.user).subscribe((resp: UserModel)=>{
+        this.user = resp
+        this.router.navigate(["/inicio"])
+      });
+      alert("Conta atualizada, faça login para continuar.");
+      environment.token="";
+      environment.name="";
+      environment.photo="";
+      environment.id= 0;
+      this.router.navigate(["/login"]);  
+    }
+  }
+
+  findByIdUser(id: number) {
+    this.authService.getByIdUser(id).subscribe((resp: UserModel)=>{
+      this.user = resp
+    })
   }
 
 }

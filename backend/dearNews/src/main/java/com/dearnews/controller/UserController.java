795beafe5961a32b061239149d73c7c4daef234b
Jhon Med <jhonatan1medeiros@gmail.com>
2022-02-03 08:57:44 -0300

    Ajustes backend

--- backend/dearNews/src/main/java/com/dearnews/controller/UserController.java
+++ backend/dearNews/src/main/java/com/dearnews/controller/UserController.java
@@ -3,11 +3,14 @@ package com.dearnews.controller;
 import java.util.List;
 import java.util.Optional;
 import javax.validation.Valid;
+
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,6 +41,11 @@ public class UserController {
 	public ResponseEntity<UsuarioLogin> login(@RequestBody Optional<UsuarioLogin> user) {
 		return usuarioService.autenticarUsuario(user).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserModel> GetById(@PathVariable long id) {
+		return userRepository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@PostMapping("/cadastrar")
--- backend/dearNews/src/main/java/com/dearnews/security/BasicSecurityConfig.java
+++ backend/dearNews/src/main/java/com/dearnews/security/BasicSecurityConfig.java
@@ -32,10 +32,18 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar")
-				.permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
-				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
-				.csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
+		.antMatchers(HttpMethod.GET, "/post").permitAll()
+		.antMatchers(HttpMethod.GET, "/post/**").permitAll()
+		.antMatchers(HttpMethod.GET, "/topic").permitAll()
+		.antMatchers(HttpMethod.GET, "/topic/**").permitAll()
+		.anyRequest().authenticated()
+		.and().httpBasic()
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().cors().and().csrf().disable();
 	}
 
 }
